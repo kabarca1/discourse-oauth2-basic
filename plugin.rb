b295901ff53193feea5d41b6536f259d611cc11c
kabarca1 <k.abarca1@hotmail.com>
2016-01-09 12:09:22 -0600

    Scope

--- plugin.rb
+++ plugin.rb
@@ -23,6 +23,7 @@ class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
                         if SiteSetting.oauth2_send_auth_header?
                           opts[:token_params] = {headers: {'Authorization' => basic_auth_header }}
                         end
+                        opts[:scope] = "profile"
                       }
   end
 
